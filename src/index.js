315dbb36fc7fc90fc9bff6f6245ae89249afa7f5
clfhhc <david.chen@minted.com>
2020-11-05 15:47:54 -0800

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('@babel/register')({ extensions: ['.js', '.ts'] });
 // import all other files after the babel hook
 // const app = require('./app');
